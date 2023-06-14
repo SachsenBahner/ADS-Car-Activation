6c81b77aabaf688aa06df710e3447e004599241a
SachsenBahner <dieolafshow@gmail.com>
2023-06-14 15:46:24 +0200

    changed to Dollar and added Struct

--- ADS_Code/ADS_Code.cpp
+++ ADS_Code/ADS_Code.cpp
@@ -5,14 +5,32 @@
 #include <fstream>
 #include <string>
 
+#include <time.h>
+
+#include <codecvt>
+
+
 
 #include "k7scan1.h"
 #include "k5huff.h"
+
 
 #pragma warning(disable:4786)
 #pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
 
 using namespace std;
+
+
+struct Buchungsanfrage {
+	string kundenName;
+	int kundenNummer;
+	time_t startZeit;
+	time_t endZeit;
+	string startpunkt;
+	string endpunkt;
+	string Kategorie;
+	int Autocodierung;
+};
 
 
 
@@ -24,6 +42,20 @@ using namespace std;
 
 int main()
 {
+	FILE *datei; 
+	char line[100];
+
+	if (fopen_s(&datei, "car.pool", "r") == 0) {
+		if (fgets(line, 100, datei) == NULL) {
+			printf("fgets error");
+		}
+		else
+			printf("%s", line);
+		fclose(datei);
+	}
+
+
+	
 
 
 	// K7scan1
@@ -59,7 +91,7 @@ int main()
 
 	// k5huff
 
-	printf("Huffman Test!\n");
+	/*printf("Huffman Test!\n");
 	CHufftree hufft;
 	hufft.BuildHeap(); 	// Heap erzeugen
 	//	hufft.pr_pq();return;
@@ -68,6 +100,23 @@ int main()
 	hufft.pr_hufftree(tree);
 	char v;
 	cin >> v;
+	*/
+
+
+	struct tm tm;
+	time_t t;
+
+	tm.tm_year = 2023 - 1900;  // Jahr abziehen, da tm_year die Anzahl der Jahre seit 1900 erwartet
+	tm.tm_mon = 6 - 1;         // Monat abziehen, da tm_mon die Anzahl der Monate seit Januar erwartet
+	tm.tm_mday = 8;
+	tm.tm_hour = 15;
+	tm.tm_min = 40;
+	tm.tm_sec = 0;
+	tm.tm_isdst = -1;          // Zeitumstellung lokal erkennen
+
+	t = mktime(&tm);
+
+	std::cout << "Time: " << t << std::endl;
 
 
 
